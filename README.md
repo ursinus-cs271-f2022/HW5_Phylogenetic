6c73f161f69ce59e8e195a14e28b36a53614bc28
Chris Tralie <chris.tralie@gmail.com>
2022-04-07 09:36:55 -0400

    Adding starter code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# HW5_Phylogenetic
+# HW5_PhylogeneticTrees
+
+COX3 amino acid sequences obtained from
+https://www.ncbi.nlm.nih.gov/nuccore/
+
+BLOSUM tables obtained from
+ftp://ftp.ncbi.nih.gov/repository/blocks/unix/blosum/blosum.tar.Z
---
+++ phylogenetic.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+def load_blosum(filename):
+    """
+    Load in a BLOSUM scoring matrix for Needleman-Wunsch
+
+    Parameters
+    ----------
+    filename: string
+        Path to BLOSUM file
+    
+    Returns
+    -------
+    A dictionary of {string: int}
+        Key is string, value is score for that particular 
+        matching/substitution/deletion
+    """
+    fin = open(filename)
+    lines = [l for l in fin.readlines() if l[0] != "#"]
+    fin.close()
+    symbols = lines[0].split()
+    X = [[int(x) for x in l.split()] for l in lines[1::]]
+    X = np.array(X, dtype=int)
+    N = X.shape[0]
+    costs = {}
+    for i in range(N-1):
+        for j in range(i, N):
+            c = X[i, j]
+            if j == N-1:
+                costs[symbols[i]] = c
+            else:
+                costs[symbols[i]+symbols[j]] = c
+                costs[symbols[j]+symbols[i]] = c
+    return costs
